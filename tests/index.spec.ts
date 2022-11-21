6ccff20d8eb1f67797ff408fd453389cbca9fc60
Cory Hutchison <cory@mutecipher.com>
2022-11-21 12:29:41 -0700

    Update index.spec.ts

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,13 +3,11 @@ import { test, expect } from '@playwright/test';
 test('title is correct', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle('Home');
+});
 
-  await page.goto('/about');
-  await expect(page).toHaveTitle('About');
-
-  await page.goto('/blog');
-  await expect(page).toHaveTitle('Blog');
-
-  await page.goto('/tags');
-  await expect(page).toHaveTitle('Tags');
+test('content is correct', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.locator('h2')).toHaveText('My awesome blog subtitle');
+  await expect(page.locator('h3')).toHaveText(/Thank you for visiting!/);
+  await expect(page.locator('button')).toHaveText('New greeting');
 });
